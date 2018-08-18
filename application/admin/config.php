c4a3bc8acd7883417853a910eb843228669cc4ad
godcop <godcoper@gmail.com>
2018-08-18 22:50:33 +0800

    第一次提交到github

---
+++ application/admin/config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yifeng
+ * Date: 2017/8/5
+ * Time: 22:37
+ */
+return [
+    'view_replace_str'  =>  [
+        '__LAYUI__'=>'/public/static/layuiadmin',
+        '__PUBLIC__'=>'/public/static/common',
+    ]
+];
