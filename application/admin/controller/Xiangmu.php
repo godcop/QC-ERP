feefb81258f2483396600c52e3ed873ffc953039
godcop <godcoper@gmail.com>
2018-08-31 18:14:39 +0800

    新增项目管理功能
    1、完善编辑功能
    2、新增项目由内嵌html改为iframe

--- application/admin/controller/Xiangmu.php
+++ application/admin/controller/Xiangmu.php
@@ -10,16 +10,8 @@ namespace app\admin\controller;
 use think\Db;
 class Xiangmu extends Common
 {
-    // 项目
+    // 默认页
     public function index(){
-        if(request()->isPost()) {
-            $data = input("post.");
-            if(db("xiangmu")->insert($data)){
-                return json_encode(200);
-            }else{
-                return json_encode(400);
-            }
-        }
         return view();
     }
 
@@ -34,10 +26,58 @@ class Xiangmu extends Common
             //计算出从那条开始查询
             $tol=($page-1)*$limit;
             // 查询出当前页数显示的数据
-            $xiangmu = db('xiangmu')->order('Id desc')->limit("$tol","$limit") -> select();
+            $xiangmu = db('xiangmu')->order("Id")->limit("$tol","$limit") -> select();
             return ["code"=>"0","msg"=>"","count"=>$count,"data"=>$xiangmu];
         }
         echo json_encode(lists());
+    }
+
+    // 新增项目
+    public function add(){
+        if(request()->isPost()) {
+            $data = input("post.");
+            if($data["xm_name"]==null || $data["xm_abbreviated"]==null){
+                return json("请完善相关信息后再提交！");
+                die;
+            }
+            if(array_key_exists("xm_status", $data)){
+                $data["xm_status"] = "销售中";
+            }else{
+                $data["xm_status"] = "已撤场";
+            }
+            //return json_encode($data);
+            if(db("xiangmu")->insert($data)){
+                return json_encode(200);
+            }else{
+                return json_encode(400);
+            }
+        }
+        return view();
+    }
+
+    // 编辑项目
+    public function edit(){
+        $id = input("param.id");
+        $editview = db("xiangmu") -> where("Id",$id) -> select();
+        if(request()->isPost()) {
+            $data = input("post.");
+            if($data["xm_name"]==null || $data["xm_abbreviated"]==null){
+                return json("请完善相关信息后再提交！");
+                die;
+            }
+            if(array_key_exists("xm_status", $data)){
+                $data["xm_status"] = "销售中";
+            }else{
+                $data["xm_status"] = "已撤场";
+            }
+            if(db("xiangmu")->where("Id",$id)->update($data)){
+                return json_encode(200);
+            }else {
+                return json_encode(400);
+            }
+        }
+        $this->assign("xm",$editview);
+        return view();
     }
 
     // 删除用户
